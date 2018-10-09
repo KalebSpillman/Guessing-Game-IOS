5b72b9c390e8455baf0b3e66bb544923d63f799b
Kaleb Spillman <kalebspillman@Interapts-MacBook-Pro-5.local>
2018-10-09 10:15:10 -0500

    Initial Commit

---
+++ Guessing Game IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guessing Game IOS
+//
+//  Created by Kaleb Spillman on 10/9/18.
+//  Copyright © 2018 Kaleb Spillman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
